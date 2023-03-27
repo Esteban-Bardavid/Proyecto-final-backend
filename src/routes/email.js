4f53ebdd3fc861e6f226267b4b6752884ad4a732
RoferEsper <roferesper@gmail.com>
2023-03-27 18:14:14 -0300

    .

--- src/routes/email.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const EmailCollection = require("../collections/email_collection");
-const AuthMiddleware = require("../middleware/auth")
-
-router.get("/", AuthMiddleware, EmailCollection.EmailGet)
-
-router.post("/", AuthMiddleware, EmailCollection.EmailPost)
-
-module.exports = router;
